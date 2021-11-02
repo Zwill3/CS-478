59deac0d656178aa8db1c00fb8de58ea0e938212
echopie72 <90227564+echopie72@users.noreply.github.com>
2021-11-01 23:19:44 -0500

    Add files via upload

---
+++ team4Chess/ChessBoardModel2/Board.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel2
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] TheGrid { get; set; }
+
+        public Board(int s)
+        {
+            Size = s;
+            TheGrid = new Cell[Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    TheGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // clear previous moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    TheGrid[i, j].LegalNextMove = false;
+                    TheGrid[i, j].CurrentlyOccupied = false;
+
+                }
+            }
+            // find legal moves
+            switch (chessPiece)
+            {
+                case "Knight":
+                    TheGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    TheGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+                case "King":
+                    break;
+                case "Rook":
+                    break;
+                case "Bishop":
+                    break;
+                case "Queen":
+                    break;
+                default:
+                    break;
+            }
+            TheGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
+        }
+
+    }
+}
